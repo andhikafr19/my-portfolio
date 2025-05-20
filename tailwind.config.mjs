dd5c7b3b88d5fffe10f22189f843c9d107a4b231
Andhika Ramadhan <andhikafirdaus@gmail.com>
2025-05-20 15:43:51 +0700

    create: component Navbar

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+// filepath: c:\Users\andhi\OneDrive\Desktop\my-portfolio\tailwind.config.mjs
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F',
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
